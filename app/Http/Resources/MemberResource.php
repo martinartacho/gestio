d79bbc44e0347693318ea887d6833ebbe4416240
Pepe <se.pepemartin@gmail.com>
2026-08-29 19:01:44 +0200

    feat(api): enriqueix MemberResource per al carnet mòbil
    - member_number_display (prefix associats_member_prefix + número)
    - org_name (nom de l'entitat)
    - city
    
    Necessari perquè el carnet de l'app mòbil pugui igualar el disseny
    del carnet web (número amb prefix ACG, nom d'entitat, ciutat).

--- app/Http/Resources/MemberResource.php
+++ app/Http/Resources/MemberResource.php
@@ -10,16 +10,19 @@ class MemberResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'            => $this->id,
-            'member_number' => $this->member_number,
-            'full_name'     => $this->full_name,
-            'first_name'    => $this->first_name,
-            'last_name'     => $this->last_name,
-            'email'         => $this->email,
-            'phone'         => $this->phone,
-            'status'        => $this->status,
-            'joined_at'     => $this->joined_at?->format('Y-m-d'),
-            'qr_token'      => $this->qr_token,
+            'id'                     => $this->id,
+            'member_number'          => $this->member_number,
+            'member_number_display'  => setting('associats_member_prefix', '') . $this->member_number,
+            'org_name'               => setting('associats_org_name', 'Entitat'),
+            'full_name'              => $this->full_name,
+            'first_name'             => $this->first_name,
+            'last_name'              => $this->last_name,
+            'email'                  => $this->email,
+            'phone'                  => $this->phone,
+            'city'                   => $this->city,
+            'status'                 => $this->status,
+            'joined_at'              => $this->joined_at?->format('Y-m-d'),
+            'qr_token'               => $this->qr_token,
         ];
     }
 }
